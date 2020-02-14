b63d8d06c7550f41086dfe4754103739a3e9c819
bryan bar <bryanbarajas96@gmail.com>
2020-02-14 11:03:39 -0800

    Sms responses generated

--- devday/routes.py
+++ devday/routes.py
@@ -1,28 +1,32 @@
-from flask import Flask, abort, request, jsonify
+from flask import Flask, abort, request, jsonify, render_template
 from devday import app, Swagger, Config
 from devday.twiliohandler import TwilioHandler
 import json
 
 handler = TwilioHandler(Config.TWILIO_ACCOUNT_SID, Config.TWILIO_AUTH_TOKEN, Config.TWILIO_NUMBER)
 appointments = {}
-msgcount = [0]
 
 
 @app.route('/sms/', methods=['POST'])
 def process_sms():
-    msgcount[0] += 1
 
     if request.headers.get('X-Twilio-Signature'):
         body = request.form['Body']
         fromnumber = request.form['From']
 
-        if 'ok' in body:
-            customermessage = 'Thank you. We shall notify you once work is complete'
-            handler.createmessage(customermessage, to=fromnumber)
+        for customer in appointments.values():
+            if customer['number'] in fromnumber:
+                if 'ok' in body.lower():
+                    customer['workApproved'] = True
+                    appointments[customer['id']] = customer
+                    customermessage = 'Thank you {}. We shall notify you once work is complete'.format(customer['name'])
+                    handler.createmessage(body=customermessage, to=fromnumber)
+                    return '<Response></Response>'
 
-        return '<Response></Response>'
+        customermessage = 'You do not have an appointment with Rebel Shop. Call to set one up.'
+        handler.createmessage(body=customermessage, to=fromnumber)
 
-    return '', 404
+    return 'Hello'
 
 
 @app.route('/setappointment/', methods=['POST'])
@@ -68,7 +72,7 @@ def set_appointment():
         customer['issue'] = 'Spark plug needs tightening'
         customer['custComment'] = 'Fix it'
         customer['techComment'] = 'Fixing your car'
-        appointments[customer['id']] = json.dumps(customer)
+        appointments[customer['id']] = customer
 
         customermessage = '{}, you have an appointment at Rebel Shop at {}, for your {}'.format(name, time, customer['car'])
 
@@ -77,29 +81,56 @@ def set_appointment():
         return json.dumps(customer)
 
 
-@app.route('/getappointment/<int:task_id>', methods=['GET'])
+@app.route('/getappointment/<int:task_id>', methods=['POST'])
 def get_appointment(task_id):
-    return appointments[task_id]
+
+    if task_id in appointments:
+        customer = appointments[task_id]
+        customer['hasArrived'] = True
+        appointments[customer['id']] = customer
+
+        car = customer['car']
+        phonenumber = customer['number']
+        customermessage = 'Welcome to the Rebel Shop Tracking.' \
+                          'You will receive updates regarding your {}'.format(car)
+        handler.createmessage(customermessage, phonenumber)
+
+        return json.dumps(appointments[customer['id']])
+
+    return 'Does not exist'
 
 
 @app.route('/approveservice/<int:task_id>', methods=['POST'])
 def approve_service(task_id):
+
     if task_id in appointments:
-        customerdata = json.loads(appointments[task_id])
-        car = customerdata['car']
-        issue = customerdata['issue']
-        price = str(customerdata['price'])
-        phonenumber = customerdata['number']
-
+        customer = appointments[task_id]
+        car = customer['car']
+        issue = customer['issue']
+        price = str(customer['price'])
+        phonenumber = customer['number']
         customermessage = 'Your {} needs {} at a cost of {}.\n' \
-                          'Please text ok to Approve or Call'.format(car, issue, price)
-
+                          'Please text ok to Approve or Call 555-555-5555'.format(car, issue, price)
         handler.createmessage(customermessage, phonenumber)
 
-    return appointments[task_id]
+    return json.dumps(appointments[task_id])
 
 
-
-@app.route('/workcomplete/<int:task_id>', methods=['GET'])
+@app.route('/workcomplete/<int:task_id>', methods=['POST'])
 def work_complete(task_id):
-    return appointments[task_id]
+
+    if task_id in appointments:
+        customer = appointments[task_id]
+        customer['isComplete'] = True
+        appointments[customer['id']] = customer
+
+        name = customer['name']
+        car = customer['car']
+        client_id = customer['id']
+        phonenumber = customer['number']
+        custommessage = 'Hello {}, you {} work is complete and ready for pick up\n' \
+                        'billing statement at: rebelshop.domain/{}{}'.format(name, car, client_id, '12345')
+        handler.createmessage(custommessage, phonenumber)
+
+        return json.dumps(customer)
+
